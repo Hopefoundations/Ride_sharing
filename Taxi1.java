f5f4c175c9797ed37af70a59f06d74c0da05472d
Tharrunya Vaisnavi P <156891044+Hopefoundations@users.noreply.github.com>
2026-07-11 11:15:41 +0530

    Update Taxi1.java
    Updated constructor to include category, driver name, and rating. Enhanced fare calculation for premium taxis.

--- Taxi1.java
+++ Taxi1.java
@@ -1,11 +1,24 @@
 class Taxi1 extends Taxi {
 
-    Taxi1(String vehicleId, double ratePerKm) {
-        super(vehicleId, ratePerKm);
+    // Constructor
+    Taxi1(String vehicleId,
+          double ratePerKm,
+          String category,
+          String driverName,
+          double rating) {
+
+        super(vehicleId,
+              ratePerKm,
+              category,
+              driverName,
+              rating);
     }
 
+    // Polymorphism
     @Override
     public double calculateFare(int distance) {
+
+        // Premium Taxi Fare
         return distance * getRatePerKm();
     }
 }
