475cfc7e9ba4f9fbf17dd692af7b81c45afb1423
Tharrunya Vaisnavi P <156891044+Hopefoundations@users.noreply.github.com>
2026-07-11 11:34:09 +0530

    Update Vehicle.java

--- Vehicle.java
+++ Vehicle.java
@@ -1,24 +1,70 @@
+// ======================================
+// Vehicle.java
+// Abstract Base Class
+// ======================================
+
 abstract class Vehicle {
 
+    // Vehicle Information
     protected String vehicleId;
     protected boolean available;
+    protected String category;
+    protected String driverName;
+    protected double driverRating;
+    protected double ratePerKm;
 
-    Vehicle(String vehicleId) {
+    // Constructor
+    Vehicle(String vehicleId,
+            String category,
+            String driverName,
+            double driverRating,
+            double ratePerKm) {
+
         this.vehicleId = vehicleId;
+        this.category = category;
+        this.driverName = driverName;
+        this.driverRating = driverRating;
+        this.ratePerKm = ratePerKm;
         this.available = true;
     }
 
+    // Getters
+
     public String getVehicleId() {
         return vehicleId;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public String getDriverName() {
+        return driverName;
+    }
+
+    public double getDriverRating() {
+        return driverRating;
+    }
+
+    public double getRatePerKm() {
+        return ratePerKm;
     }
 
     public boolean isAvailable() {
         return available;
     }
 
+    // Setters
+
+    public void setDriverRating(double driverRating) {
+        this.driverRating = driverRating;
+    }
+
     public void setAvailable(boolean available) {
         this.available = available;
     }
+
+    // Abstract Method
 
     public abstract double calculateFare(int distance);
 }
