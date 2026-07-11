b38b1b7170315f27516b46f242ec3e22d518ca9a
Tharrunya Vaisnavi P <156891044+Hopefoundations@users.noreply.github.com>
2026-07-11 11:14:06 +0530

    Update Taxi.java
    Added new fields for category, driver name, and rating with corresponding getters and setters.

--- Taxi.java
+++ Taxi.java
@@ -1,20 +1,66 @@
 class Taxi extends Vehicle {
 
     private double ratePerKm;
+    private String category;
+    private String driverName;
+    private double rating;
 
-    Taxi(String vehicleId, double ratePerKm) {
+    // Constructor
+    Taxi(String vehicleId,
+         double ratePerKm,
+         String category,
+         String driverName,
+         double rating) {
+
         super(vehicleId);
+
         this.ratePerKm = ratePerKm;
+        this.category = category;
+        this.driverName = driverName;
+        this.rating = rating;
     }
 
+    // Getter for Fare
     public double getRatePerKm() {
         return ratePerKm;
     }
 
+    // Setter for Fare
     public void setRatePerKm(double ratePerKm) {
         this.ratePerKm = ratePerKm;
     }
 
+    // Getter for Category
+    public String getCategory() {
+        return category;
+    }
+
+    // Setter for Category
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    // Getter for Driver Name
+    public String getDriverName() {
+        return driverName;
+    }
+
+    // Setter for Driver Name
+    public void setDriverName(String driverName) {
+        this.driverName = driverName;
+    }
+
+    // Getter for Rating
+    public double getRating() {
+        return rating;
+    }
+
+    // Setter for Rating
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+    // Calculate Fare
     @Override
     public double calculateFare(int distance) {
         return distance * ratePerKm;
